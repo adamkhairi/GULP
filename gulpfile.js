775802f019cb75528ebd12f5ac73bb9c9e6aa71c
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2020-04-10 20:49:18 +0100

    Template update gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // Initialize modules
 // Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
-const { src, dest, watch, series, parallel } = require("gulp");
+const {src, dest, watch, series, parallel} = require("gulp");
 
 // Importing all the Gulp-related packages we want to use
 const sourcemaps = require("gulp-sourcemaps");
@@ -13,39 +13,39 @@ const cssnano = require("cssnano");
 
 // File paths
 const files = {
-  scssPath: "app/scss/**/*.scss",
-  jsPath: "app/js/**/*.js"
+	scssPath: "app/scss/**/*.scss",
+	jsPath: "app/js/**/*.js"
 };
 
 // Sass task: compiles the style.scss file into style.css
 function scssTask() {
-  return src(files.scssPath)
-    .pipe(sourcemaps.init()) // initialize sourcemaps first
-    .pipe(sass().on("error", sass.logError)) // compile SCSS to CSS
-    .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
-    .pipe(sourcemaps.write(".")) // write sourcemaps file in current directory
-    .pipe(dest("dist/css")); // put final CSS in dist folder
+	return src(files.scssPath)
+	.pipe(sourcemaps.init()) // initialize sourcemaps first
+	.pipe(sass().on("error", sass.logError)) // compile SCSS to CSS
+	.pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+	.pipe(sourcemaps.write(".")) // write sourcemaps file in current directory
+	.pipe(dest("dist/css")); // put final CSS in dist folder
 }
 
 // JS task: concatenates and uglifies JS files to script.js
 function jsTask() {
-  return src([
-    files.jsPath
-    //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
-  ])
-    .pipe(concat("main.js"))
-    .pipe(terser())
-    .pipe(dest("dist/js"));
+	return src([
+		files.jsPath
+		//,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+	])
+	.pipe(concat("main.js"))
+	.pipe(terser())
+	.pipe(dest("dist/js"));
 }
 
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
 function watchTask() {
-  watch(
-    [files.scssPath, files.jsPath],
-    { interval: 1000, usePolling: true }, //Makes docker work
-    series(parallel(scssTask, jsTask))
-  );
+	watch(
+		[files.scssPath, files.jsPath],
+		{interval: 1000, usePolling: true}, //Makes docker work
+		series(parallel(scssTask, jsTask))
+	);
 }
 
 // Export the default Gulp task so it can be run
